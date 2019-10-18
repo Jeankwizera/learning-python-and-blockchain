f7083c89e1368a8163b86fd920c584f1202dcd72
Kwizera Jean Victor <jean4hit@gmail.com>
2019-10-18 11:08:25 +0300

    refined

--- first blockchain.py
+++ first blockchain.py
@@ -3,50 +3,90 @@ blockchain = ["block 1"]
 block = ["trans 1"]
 invoices = [0]
 
-def send_money( ):   
-    
-    amount = float(input("Enter Amount To Send: "))
-    sender_email = input("Enter Sender Email: ")
-    recepient_email = input("Enter Recepient Email: ")
-    
-    purpose = input("Enter Reason For Payment: ")
+sender = ["jeanK", 1000]
+recepient = ["MeApp", 97]
 
-    sender_balance = [100]
-    recepient_balance = [100]
+def transact(invoices):
     
-    new_sender_balance = sender_balance[-1] - amount
-    sender_balance.append(new_sender_balance)
-    
-    new_recepient_balance = recepient_balance[-1] + amount
-    recepient_balance.append(new_recepient_balance)
-    
+    receipt = []
     invoice_number = invoices[-1] + 1
     invoices.append(invoice_number)
+    receipt.append(invoice_number)
+        
+    sender_address = sender[0]
+    receipt.append(sender_address)
+        
+    sender_balance = sender[-1]
+        
+    amount = float(input("Enter Amount To Send: "))
+    receipt.append(amount)
+        
+    new_sender_balance = sender_balance - amount
+    sender.append(new_sender_balance)
+        
+    recepient_address = recepient[0]
+    receipt.append(recepient_address)
+        
+    recepient_balance = recepient[-1]
+    new_recepient_balance = recepient_balance + amount
+    recepient.append(new_recepient_balance)
+        
+    purpose = input("Enter Reason For Payment: ")
+    receipt.append(purpose)
     
-    transaction = print(["using", invoice_number, sender_email, 
-                       "sends", amount, "remains", 
-                       sender_balance[-1], "to", recepient_email,
-                       "becomes", recepient_balance[-1], "for",
-                       purpose])
-    return(transaction)
+    print(receipt)
+    comment()
+    
+    block.append(receipt)
+    
+def hashing(block):
+    block = print(block)
+    block_hash = hash(block)
+    return(block_hash)
 
-def add_transaction_to_block( ):
-    transaction = send_money( )
-    block.append(transaction)
-    return(block)
-
-def get_last_block( ):
-    last_block = blockchain[-1]
-    return(last_block)
+def comment():
+    comm = input("Satisfied?: ")
+    block.append(comm)
     
-def transact( ):
-    block = add_transaction_to_block( )
-    blockchain.append([get_last_block( ), block])
-    return(blockchain)
+def get_last_block(blockchain):
+        return(blockchain[-1])
+   
+def add_block_to_blockchain():
+    last_block = get_last_block(blockchain)
+    print("Block Full")
     
-    print(add_transaction_to_block( ))
+    block_hash = hashing(block)
+    
+    print("Adding Block to Blockchain")
+    blockchain.append([last_block, block_hash])
     print(blockchain)
+    send_money()    
+                    
+def transactions():
     
+    transact(invoices)
+    print(sender)
+    print(recepient)
+    
+    transact(invoices)
+    print(sender)
+    print(recepient)
+
+    transact(invoices)
+    print(sender)
+    print(recepient)
+    
+    
+    
+def send_money():
+    transactions()
+    add_block_to_blockchain()
+
+send_money()
+    
+    
+        
+
     
     
     
